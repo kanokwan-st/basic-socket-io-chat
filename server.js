ae2735f2f0274506ef06bdaae32c464fa40b1e1d
kanokwan-st <ganokwn123@gmail.com>
2025-07-16 02:05:40 +0700

    Add comment for server.js and index.html

--- server.js
+++ server.js
@@ -4,30 +4,37 @@ import { fileURLToPath } from "url";
 import { dirname, join } from "path";
 import { Server } from "socket.io";
 
-const app = express();
-const server = createServer(app);
-const io = new Server(server);
+const app = express();              // Create Express app
+const server = createServer(app);   // Create HTTP server
+const io = new Server(server);      // Attach Socket.io to the server
 
+// Get the current directory name
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-// Add endpoint
+// Send HTML file when visiting root URL
 app.get("/", (req, res) => {
   res.sendFile(join(__dirname, "index.html"));
 });
 
+// Socket.io connection event (when a client connects) 
+// (socket) represents a client that just connected
+// io is the Socket.IO server instance (manages all clients).
 io.on("connection", (socket) => {
   console.log("User connected");
-  // when disconnect
+
+  // Client disconnect
   socket.on("disconnect", () => {
     console.log("User disconnected");
   });
-  // When send message
+
+  // Listen for chat messages from client
   socket.on("chat message", (msg) => {
     console.log("message: " + msg);
-    io.emit('chat message', msg);
+    io.emit('chat message', msg);   // Broadcast message to all clients
   });
 });
 
+// Start the server on port 3000
 server.listen(3000, () =>
   console.log(`Server is running at http://localhost:3000`)
 );
